04490e4b0770f4495365efcbc89f02789963c295
ysdxz207 <ysdxz207@qq.com>
2017-05-03 11:49:13 +0800

    rename web application-context.xml to context.xml

--- tnews-web/src/test/java/TestInterface.java
+++ tnews-web/src/test/java/TestInterface.java
@@ -4,7 +4,7 @@ import java.nio.file.Paths;
 
 /**
  * 函数式接口演示
- * 一个所谓的函数式接口必须要有且仅有一个抽象方法声明
+ * 函数式接口必须要有且仅有一个抽象方法声明
  * @author weishaoqiang
  * @date 2017-04-26 14:42
  */
@@ -24,6 +24,8 @@ public class TestInterface {
 		Integer converted = converter.convert("123");
 		System.out.println(converted);
 
-		Files.lines(Paths.get("D:\\workspace\\idea\\tnews\\tnews-web\\src\\test\\java\\TestInterface.java")).map(String::trim).forEach(System.out::println);
+		Files.lines(Paths.get(
+				"D:\\workspace\\idea\\tnews\\tnews-web\\src\\test\\java\\TestInterface.java"))
+				.map(String::trim).map((s) -> s.indexOf("main") > -1).forEach(System.out::println);
 	}
 }
