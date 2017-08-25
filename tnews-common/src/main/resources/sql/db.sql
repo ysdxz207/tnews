8264737f2c2ed9ae07641d1ad295f95c630cc822
ysdxz207 <ysdxz207@qq.com>
2017-08-25 23:00:42 +0800

    sql and comment

---
+++ tnews-common/src/main/resources/sql/db.sql
@@ -0,0 +1,4 @@
+create database db_tnews_news DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
+create database db_tnews_member DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
+create database db_tnews_dictionary DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
+create database db_tnews_manager DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
